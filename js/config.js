6a06d1b508d001d14c9c353f1ed4710842386579
Josybett Campos <josycamgon@hotmail.com>
2018-09-28 18:13:04 -0500

    config api/

--- js/config.js
+++ js/config.js
@@ -16,7 +16,7 @@ function timerIncrement() {
         window.location.href = 'login.php';
     }
 }
-var url = 'http://www.proconty.com/API/distrifar/'; //'www.proconty.com/API/distrifar/'; 
+var url = 'api/'; //'www.proconty.com/API/distrifar/'; 
 var usuario = "";
 
 function alertar(tipo, titulo, mensaje) {
