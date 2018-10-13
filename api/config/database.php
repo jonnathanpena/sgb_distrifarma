85360e4863e0c0a365acdac4f8a88a387553897f
Jonnathan Peña <razaisger@hotmail.com>
2018-10-13 15:34:43 -0500

    listo guia de entrega

--- api/config/database.php
+++ api/config/database.php
@@ -7,8 +7,8 @@ class Database{
     // specify your own database credentials
     private $host = "localhost";
     private $db_name = "sgipro_distrifarma"; //"proco389_distrifar";
-    private $username = "sgipro_dfarma";//"proco389_dfar";
-    private $password = "SGI_390";//"Distrifar2018";
+    private $username = "root";//"proco389_dfar";
+    private $password = "";//"Distrifar2018";
     public $conn;
  
     // get the database connection
